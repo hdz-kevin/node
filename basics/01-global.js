f3862ae330be1621e2295f5ec9a40de9836ef87f
Kevin Hernandez <kevinhdez592@gmail.com>
2026-05-16 23:09:57 -0600

    globalThis object

---
+++ basics/01-global.js
@@ -0,0 +1,12 @@
+
+// el objeto global en node es global y en el navegador es window
+// globalThis es el objeto global en cualquier entorno de javascript
+
+// En el navegador globalThis apunta a window
+// En node globalThis apunta a global
+
+globalThis.console.log("hello world")
+
+console.log("hello world 2");
+
+
